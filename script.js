9d85b7da78394388dbfa542a915bfe3b38eff219
dominikdobos <dominik.dobos33@gmail.com>
2024-02-29 12:16:21 +0100

    órai

--- script.js
+++ script.js
@@ -1,15 +1,62 @@
 window.addEventListener("load", function () {
-    elemekElerese1();
-    elemekElerese2();
-    elemekElerese3();
+  elemekElerese1();
+  elemekElerese2();
+  elemekElerese3();
+  elemekElerese4();
+  elemekFormazasa1();
+  esemenykezeles1();
+  esemenykezeles2();
+  esemenykezeles3();
 });
 
 function elemekElerese1() {
-    /**Ide jön az első feladat */
+  const ELEM = document.querySelectorAll("section h2")[0];
+  console.log(ELEM.innerHTML);
 }
-function elemekElerese1() {
-    /**Ide jön az 2.  feladat */
+function elemekElerese2() {
+  const ELEM = document.querySelectorAll("#ide")[0];
+  ELEM.innerHTML = "Jó reggelt!";
 }
-function elemekElerese1() {
-    /**Ide jön az 3. feladat */
+function elemekElerese3() {
+  const ELEM = document.querySelectorAll(".ide")[0];
+  ELEM.innerHTML = "Jó reggelt!";
 }
+function elemekElerese4() {
+  const LISTA = [];
+  const LISTA_ELEM = document.querySelector(".lista");
+  let szoveg = "<ul>";
+  for (let index = 0; index < 5; index++) {
+    let rnd = Math.floor(Math.random() * (31 - 10) + 10);
+    LISTA[index] = rnd;
+    szoveg += `<li>${LISTA[index]}</li>`;
+  }
+  szoveg += "</ul>";
+  LISTA_ELEM.innerHTML = szoveg;
+}
+function elemekFormazasa1() {
+  const LISTA_ELEM = document.querySelector(".lista");
+  LISTA_ELEM.classList.add("formazott");
+}
+function esemenykezeles1() {
+  const LISTA_ELEM = document.querySelector(".lista");
+  const KATTINTAS_UTAN_ELEM = document.querySelector(".kattintasutan");
+  LISTA_ELEM.addEventListener("click", function () {
+    KATTINTAS_UTAN_ELEM.innerHTML = LISTA_ELEM.innerHTML;
+  });
+}
+function esemenykezeles2() {
+  const FELADAT_ELEM = document.querySelectorAll(".feladat")[0];
+  FELADAT_ELEM.innerHTML = "<button id='feladat_btn'>Nyomd meg</button>";
+  const GOMB_ELEM = document.querySelector("#feladat_btn");
+  GOMB_ELEM.addEventListener("click", function () {
+    FELADAT_ELEM.innerHTML += `<div><img id="kep" src="./kep.jpg" alt="kep" /></div>`;
+  });
+}
+function esemenykezeles3() {
+  const KEP_ELEM = document.querySelector(".feladat img");
+  KEP_ELEM.addEventListener("mouseover", function () {
+    KEP_ELEM.classList.add("kis_kep");
+    console.log("asd");
+  });
+}
+function esemenykezeles4() {}
